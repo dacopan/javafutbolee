ee6152a8a20385a210270cfe41497c46d7ea5529
dacopan <dacopanbsc@gmail.com>
2015-12-28 11:06:19 -0500

    fixes #11

--- javafutbolee-web/src/main/webapp/assets/js/widgets/inputswitch.js
+++ javafutbolee-web/src/main/webapp/assets/js/widgets/inputswitch.js
@@ -1,6 +1,7 @@
 /**
  * PrimeFaces InputSwitch Widget
  */
+var idx = 1;
 PrimeFaces.widget.InputSwitch = PrimeFaces.widget.DeferredWidget.extend({
     init: function (cfg) {
         this._super(cfg);
@@ -10,9 +11,10 @@ PrimeFaces.widget.InputSwitch = PrimeFaces.widget.DeferredWidget.extend({
         this.offContainer = this.jq.children('.ui-inputswitch-off');
         this.offLabel = this.offContainer.children('span');
         this.handle = this.jq.children('.ui-inputswitch-handle');
-        this.input = $(this.jqId + '_input');
-
+        this.input = $(this.jqId + '_input');        
         this.renderDeferred();
+        this.idx = idx++;
+        //console.log('jfee: init '+ this.jqId + ' unique: ' + this.idx);
     },
     _render: function () {
         /*        var onContainerWidth = this.onContainer.width(),
@@ -51,16 +53,22 @@ PrimeFaces.widget.InputSwitch = PrimeFaces.widget.DeferredWidget.extend({
         }
         //*/
 
-
+        //console.log('jfee: _render '+ this.jqId + ' unique: ' + this.idx);
         if (!this.input.prop('disabled')) {
             this._bindEvents();
         }
     },
     _bindEvents: function () {
         var $this = this;
-
+        //console.log('jfee: _bindEvents ' + $this.jqId + ' unique: ' + $this.idx);
+        //prevent duplicates
+        this.jq.off( ".inputSwitch" );
         this.jq.on('click.inputSwitch', function (e) {
+            //console.log(e.target);
+            //console.log('jfee: click {' + this.jq + '} ' + $this.jqId + ' unique: ' + $this.idx);
+            //console.log($this.jq);
             $this.toggle();
+            $this.input.trigger('focus');
         });
 
         this.input.on('focus.inputSwitch', function (e) {
@@ -84,6 +92,8 @@ PrimeFaces.widget.InputSwitch = PrimeFaces.widget.DeferredWidget.extend({
                     }
                 })
                 .on('change.inputSwitch', function (e) {
+                    //console.log(e.target);
+                    //console.log('jfee: change {' + $(e.target).attr("class") + '} ' + $this.jqId + ' unique: ' + $this.idx);
                     if ($this.input.prop('checked'))
                         $this._checkUI();
                     else
@@ -91,19 +101,22 @@ PrimeFaces.widget.InputSwitch = PrimeFaces.widget.DeferredWidget.extend({
                 });
     },
     toggle: function () {
+        //console.log('jfee: toogle ' + this.jqId);
         if (this.input.prop('checked'))
             this.uncheck();
         else
             this.check();
     },
     check: function () {
+        //console.log('jfee: check ' + this.jqId);
         this.input.prop('checked', true).trigger('change');
     },
     uncheck: function () {
+        //console.log('jfee: uncheck ' + this.jqId);
         this.input.prop('checked', false).trigger('change');
     },
     _checkUI: function () {
-        console.log("checked");
+        //console.log('jfee: checkui ' + this.jqId);
         this.jq.addClass("ui-state-checked");
         //this.onContainer.animate({width: this.offset}, 200);
         //this.onLabel.animate({marginLeft: 0}, 200);
@@ -112,6 +125,7 @@ PrimeFaces.widget.InputSwitch = PrimeFaces.widget.DeferredWidget.extend({
     },
     _uncheckUI: function () {
         //this.onContainer.animate({width: 0}, 200);
+        //console.log('jfee: uncheckui ' + this.jqId);
         this.jq.removeClass("ui-state-checked");
         //this.onLabel.animate({marginLeft: -this.offset}, 200);
         //this.offLabel.animate({marginRight: 0}, 200);
